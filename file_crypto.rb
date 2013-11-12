4782157024591c4d410f400336c28965c0c59272
tokihiro <tokihiro@h122.n85.ibaraki.ac.jp>
2013-11-12 13:33:32 +0900

    add file_crypto.rb

--- file_crypto.rb
+++ file_crypto.rb
@@ -1,39 +1,76 @@
-# -*- coding: ascii-8bit -*-
+# -*- coding: utf-8 -*-
+=begin
+
+ファイルの暗号化
+
+=end
+
 require 'openssl'
-require 'bin_dump.rb'
+require './create_key'
 
-def encrypt(file, key, iv)
-  enc = OpenSSL::Cipher::Cipher.new("aes-128-cbc") #128bitのAES暗号をCBCモードで使うインスタンス生成
-  enc.encrypt                                      #暗号化モード
-  enc.key = key
-  enc.iv = iv  
-  return enc.update(file) + enc.final                 #暗号化
+class File_Crypto
+
+  def initialize(key = nil, iv = nil, alg = "aes-128-cbc")
+    @cip = OpenSSL::Cipher.new(alg)
+    @key = key
+    @iv = iv
+  end
+
+  def file_encrypt(filepath)
+    if @key != nil && @iv != nil
+      @cip.encrypt #暗号化モード
+      @cip.key = @key #鍵セット
+      @cip.iv = @iv #イニシャライズベクタセット
+      @cip.padding = 1 #パディング有効
+      enc_data = ""
+      File.open(filepath){|f|
+      while data = f.read(1024)
+        enc_data << @cip.update(data)
+      end
+      }
+      enc_data << @cip.final
+      return enc_data
+    else
+      return nil
+    end
+  end
+
+  def file_decrypt(filepath)
+    if @key != nil && @iv != nil
+      @cip.decrypt #復号モード
+      @cip.key = @key #鍵セット
+      @cip.iv = @iv #イニシャライズベクタセット
+      @cip.padding = 1 #パディング有効
+      dec_data = ""
+      File.open(filepath){|f|
+      while data = f.read(1024)
+        dec_data << @cip.update(data)
+      end
+      }
+      dec_data << @cip.final
+      return dec_data
+    else
+      return nil
+    end
+  end
+
 end
 
-def decrypt(file, key, iv)
-  dec = OpenSSL::Cipher::Cipher.new("aes-128-cbc") #128bitのAES暗号をCBCモードで使うインスタンス生成
-  dec.decrypt                                      #復号モード
-  dec.key = key
-  dec.iv = iv
-  return dec.update(file) + dec.final                 #復号
-end
- 
-if __FILE__ == $0                                  #このプログラムが単体で実行される場合のみ以下を実行
-  plaintext = ARGV[0]
-  io = File.open('k1.txt', 'rb')
-  io2 = File.open('iv.txt', 'rb')  
-  inFile = File.open(plaintext, 'rb');
-  key = io.gets
-  iv = io2.gets
-  print "[key] : "
-  bin_dump(key, 16)
-  print "[iv] : "
-  bin_dump(iv, 16)
-  
-  outFile = encrypt inFile, key, iv                     #__FILE__ と $0は他のプログラムにrequireされると一致しない。
-  print "[ciphertext] : "
-  bin_dump(ciphertext, 16)
-  b = decrypt ciphertext, key, iv
-  p b
-end
 
+#このプログラムが単体で実行される場合のみ以下を実行
+if __FILE__ == $0
+  filepath = ARGV[0]
+  ck = Create_key.new("aes-128-cbc")
+  key = ck.key_gen
+  iv = ck.iv_gen
+
+  c = File_Crypto.new(key, iv, "aes-128-cbc")
+  cipherdata = c.file_encrypt(filepath)
+  if cipherdata != nil
+    File.binwrite("Enc1", cipherdata);
+    b = c.file_decrypt("./Enc1")
+    File.write("hoge.txt", b);
+  else
+    print "error\n"
+  end
+end
