ee540424a0b9df915c0e8f3171b1725c5e07d2f3
Hirotoki <tokihiro.k@gmail.com>
2014-02-11 16:48:23 +0900

    add opencv threadfold test

---
+++ threshold.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'opencv'
+include OpenCV
+
+mat = CvMat.new(3, 3, CV_8U, 1)
+mat.set_data([1, 2, 3, 4, 5, 6, 7, 8, 9])
+# mat #=> [1, 2, 3,
+#          4, 5, 6,
+#          7, 8, 9]
+
+result = mat.threshold(4, 7, CV_THRESH_BINARY)
+
+# result #=> [0, 0, 0,
+#             0, 7, 7,
+#             7, 7, 7]
+p result
